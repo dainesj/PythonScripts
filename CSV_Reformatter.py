eef49a14aaf1ca1bbdfa92a6612dd38d6cbc4a9a
jriester <67034998+jriester@users.noreply.github.com>
2023-08-29 15:44:31 -0400

    Update CSV_Reformatter.py
    Removed edge checking functions and opted to catch the errors instead.

--- CSV_Reformatter.py
+++ CSV_Reformatter.py
@@ -17,13 +17,6 @@ def parse_args() -> str:
     return file_path
 
 
-# Check that the index requested by the user is more than 0 and less than the highest column index
-def check_bad_edges(num: int, column_length: int) -> bool:
-    if num < 0 or num > column_length:
-        print(f"You entered an invalid character\nBad number: {num} | Largest possible index: {column_length - 1}")
-        return False
-
-
 # Show the user all the column indexes and names, ask them which they want
 # Take the list, strip all whitespace, split on comma
 # Check for bad edge cases
@@ -33,33 +26,30 @@ def column_menu(tup_list: tuple, column_length: int) -> list:
     print(*tuple_list, sep='\n')
 
     column_list = input("Enter the column index you'd like to grab\n")
-    input_list = [x for x in column_list.replace(" ", "").split(',')]
-
-    for n in re.split(",|-", ",".join(input_list)):
-        if check_bad_edges(int(n), column_length) is False:
-            print("You entered a number either greater than or less than the length of the columns\nTry again dummy, menu reappearing in 5 seconds, take this time to think about what you did")
-            time.sleep(5)
-            column_menu(tup_list, column_length)
-
-    return input_list
+    return [x for x in column_list.replace(" ", "").split(',')]
 
 
 # Take list of user input, parse ranges and individual indexes, append to list, return list
 def parse_user_list(user_indexes: list) -> list:
     output_list = []
-    for x in user_indexes:
-        if '-' in x:
-            s = x.split("-")
-            if int(s[-1]) == int(s[0]):
-                continue
-            if int(s[-1]) > int(s[0]):
-                for n in range(int(s[0]), int(s[-1]) + 1):
-                    output_list.append(n)
+    try:
+        for x in user_indexes:
+            if '-' in x:
+                s = x.split("-")
+                if int(s[-1]) == int(s[0]):
+                    continue
+                if int(s[-1]) > int(s[0]):
+                    for n in range(int(s[0]), int(s[-1]) + 1):
+                        output_list.append(n)
+                else:
+                    for n in reversed(range(int(s[-1]), int(s[0]) + 1)):
+                        output_list.append(n)
             else:
-                for n in reversed(range(int(s[-1]), int(s[0]) + 1)):
-                    output_list.append(n)
-        else:
-            output_list.append(int(x))
+                output_list.append(int(x))
+    except ValueError as e:
+        print(f"This script expects positive integers, commas, or hyphens.\n {e}")
+        exit()
+
     return output_list
 
 
@@ -78,7 +68,12 @@ def grab_columns(dataframe: pd.DataFrame) -> list:
 
 # Take in user's requested indexes, grab those from the dataframe, write the dataframe to a CSV at present working directory with _dataframe.csv postfix
 def list_to_dataframe(in_list: list, dataf: pd.DataFrame, path: str):
-    dataf = dataf[dataf.columns[in_list]]
+    try:
+        dataf = dataf[dataf.columns[in_list]]
+    except IndexError as e:
+        print(f"Invalid index: {e}")
+        exit()
+
     dataf.to_csv(f"{path}_dataframe.csv", sep='\t', index=False)
     if os.path.exists(f"{path}_dataframe.csv"):
         print(f"Created file: {path}_dataframe.csv")
